db764b30a4ee18ef930562f5ba372a983538f496
Rishi <ambwanirishi@gmail.com>
2023-11-01 14:34:44 +0530

    Update load_jobs.py

--- load_jobs.py
+++ load_jobs.py
@@ -6,7 +6,15 @@ from src.db.crud import read_record
 
 
 class LoadJobs:
+    """
+    Class designed to handle the creation of jobs using business and image data obtained 
+    from various types of data sources
+    """
     def __init__(self):
+        """
+        Initializes LoadJobs with pre-defined job mappings 
+        and default schedule name.
+        """
         self.default_schedule_name = "Hourly"
         self.jobs_mapping = {
             "biz_to_cde": {"source": "biz_connection", "target": "CDE-BIZ_CONN"},
@@ -20,6 +28,12 @@ class LoadJobs:
         }
 
     async def get_default_job_schedule_pk(self):
+        """
+        Retrieves and returns the primary key of the predefined default job schedule.
+        
+        Returns:
+            job_schedule_pk (str): primary key of the default job schedule
+        """
         job_schedule = await read_job_schedule(
             self.default_schedule_name, "schedule_name"
         )
@@ -27,6 +41,15 @@ class LoadJobs:
         return job_schedule.job_schedule_pk
 
     async def get_connection_pk(self, connection_name):
+        """
+        Retrieves and returns the primary key of a connection based on its name.
+        
+        Args: 
+            connection_name (str): name of the specific connection.
+        
+        Returns:
+            connection_pk (str): primary key of the connection.
+        """
         connection_record = await read_record(
             connection_name, "connection", "connection_name"
         )
@@ -36,10 +59,27 @@ class LoadJobs:
         return connection_obj.connection_pk
 
     async def create_jobs_from_data(self, jobs_data):
+        """
+        Creates jobs using the provided data.
+        
+        Args:
+            jobs_data (list): list of dictionaries containing job data.
+        """
         for job_data in jobs_data:
             await create_job_with_config(**job_data)
 
     async def dynamically_populate_job_data(self, job_name, data):
+        """
+        Populates necessary job data dynamically based on job name and the
+        data provided.
+        
+        Args:
+            job_name (str): name of the job.
+            data (dict): data to be utilised for job data population.
+        
+        Returns:
+            (dict): dynamically populated job data.
+        """
         unique_id = (
             data["connection_pk"] if data.get("connection_pk") else data["collage_pk"]
         )
@@ -55,6 +95,12 @@ class LoadJobs:
         }
 
     async def create_job_post_collage(self, collage):
+        """
+        Creates a job after completion of a collage task.
+        
+        Args:
+            collage (obj): collage object.
+        """
         job_data = await self.dynamically_populate_job_data(
             "dis_to_cde", collage.__dict__
         )
@@ -68,6 +114,12 @@ class LoadJobs:
         await self.create_jobs_from_data([job_data])
 
     async def create_job_post_bizz_connection(self, connections_data):
+         """
+        Creates a job after a business data connection is established.
+        
+        Args:
+            connections_data (obj): business connection data.
+        """
         job_data = await self.dynamically_populate_job_data(
             "biz_to_cde", connections_data.__dict__
         )
@@ -84,6 +136,13 @@ class LoadJobs:
         await self.create_jobs_from_data([job_data])
 
     async def create_job_post_image_connection(self, connections_data, user_guid):
+         """
+        Creates a job after an image data connection is established.
+        
+        Args:
+            connections_data (obj): image connection data.
+            user_guid (str): Guid of the user for whom the job is to be created.
+        """
         image_to_cde_job = await self.dynamically_populate_job_data(
             "image_to_cde", connections_data.__dict__
         )
@@ -127,6 +186,13 @@ class LoadJobs:
         )
 
     async def create_job_post_connection(self, connections_data, user_guid):
+        """
+        Creates a job after a specified type of connection (business or image) is established. 
+        
+        Args: 
+            connections_data (obj): connection data.
+            user_guid (str): Guid of the user for whom the job is to be created.
+        """
         process_type = await select_process_type(connections_data.process_type_fk)
         process_type_name = process_type.process_type_name
 
